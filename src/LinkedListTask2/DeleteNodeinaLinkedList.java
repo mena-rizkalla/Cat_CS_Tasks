ae12f3dc3319c3cbb69601c39a61baf4b3d1b90e
mena <mena.rizkalla1@gmail.com>
2024-02-10 13:32:43 +0200

    delete node

---
+++ src/LinkedListTask2/DeleteNodeinaLinkedList.java
@@ -0,0 +1,16 @@
+package LinkedListTask2;
+
+public class DeleteNodeinaLinkedList {
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+
+    public void deleteNode(ListNode node) {
+
+        node.val = node.next.val;
+        node.next = node.next.next;
+
+    }
+}
