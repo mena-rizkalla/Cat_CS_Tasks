7aa0081770977e379a66565a02d5e39b4504b166
mena <mena.rizkalla1@gmail.com>
2024-02-14 15:46:57 +0200

    LRU Cache

--- src/main.java
+++ src/main.java
@@ -1,10 +1,25 @@
 import ArrayTask1.GroupAnagrams;
+import DoubleLinkedList.BrowserHistory;
+import DoubleLinkedList.LRUCache;
 import LinkedListTask2.SwapNodes;
 
 public class main {
 
 
     public static void main(String[] args){
+
+
+        BrowserHistory obj = new BrowserHistory("Leetcode.com");
+        obj.visit("google.com");
+        obj.visit("facebook.com");
+        obj.visit("youtube.com");
+       String param_2 = obj.back(1);
+        String param_3 = obj.back(1);
+        String param_4 = obj.forward(1);
+        obj.visit("LinkedIN");
+        String param_5 = obj.forward(2);
+        String param_6 = obj.back(2);
+        String param_7 = obj.back(7);
 
 
         System.out.println("mena");
